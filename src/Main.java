a1ceb737b5c3f22fbdcaf575579bb696d6cb8520
Arthur-Nevess <arthurneves0a12@gmail.com>
2024-07-29 22:07:44 -0300

    Primeiro exercicio complero

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Nomes nome1 = new Nomes("Jaqueline");
+        Nomes nome2 = new Nomes("Larissa");
+        Nomes nome3 = new Nomes("Leone");
+        Nomes nome4 = new Nomes("Louize");
+        Nomes nome5 = new Nomes("Arthur");
+        ArrayList<Nomes> nome = new ArrayList<>();
+        nome.add(nome1);
+        nome.add(nome2);
+        nome.add(nome3);
+        nome.add(nome4);
+        nome.add(nome5);
+
+        nome.forEach( Nomes -> System.out.println(Nomes));
+
+
+
     }
 }
---
+++ src/Nomes.java
@@ -0,0 +1,20 @@
+public class Nomes {
+    private String nome;
+
+    public Nomes(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
